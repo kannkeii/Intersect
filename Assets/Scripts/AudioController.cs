0d209f4f0eb9d3ecb20582bad22bc85126cc6938
kannkeii <kannkeii@outlook.jp>
2023-09-05 15:10:02 +0900

    [add]共通_Sound関連、TitleScene_消音ボタン作成、TitleScene_開始文字作成、TitleScene_終了ボタン作成、TitleScene_タイトル文字作成

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    public static AudioController Instance { get { return instance; } set { instance = value; } }
+
+    private static AudioController instance = null;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("AudioControllerのインスタンスが失敗しました。");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool GetStatus()
+    {
+        return audioSource.mute;
+    }
+
+    public void Change()
+    {
+        audioSource.mute = !audioSource.mute;
+    }
+}
